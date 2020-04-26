6bb830c538f59c6de709c47215ecfcace795aef1
Kushang Gajjar <kgajjar@salesforce.com>
2020-04-26 17:11:19 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3235091b8ad2d1f1d29e.js"
+    "url": "webpack-runtime-7d02526cf5925cb316e0.js"
   },
   {
     "url": "framework-37b45856124a722f0eb5.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cc8986e7c0f0f54c529ed5a00e5be5c9"
+    "revision": "28ff35d7d4471af157369ebb92e832d5"
   },
   {
     "url": "static/webfonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"

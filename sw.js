ddf4cd404a1095c90151ccda05963fa6d8ebe908
Kushang Gajjar <g.kushang@gmail.com>
2020-08-30 19:28:10 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-46ef2e9c5a32e0252438.js"
+    "url": "webpack-runtime-f3a77c482e5fe92ffb50.js"
   },
   {
     "url": "framework-37b45856124a722f0eb5.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8ca89d8248aacae451ecf598a0210c71"
+    "revision": "c56bbc997973b565b3db366392f2cc03"
   },
   {
     "url": "static/webfonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"

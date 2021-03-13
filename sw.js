2ca3ab3521563c30c8e5a38184c5a38b926dab1d
Kushang <kgajjar@salesforce.com>
2021-03-13 18:11:10 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cece0fe5dc0e45425296.js"
+    "url": "webpack-runtime-7a99529b8ac6300fcff6.js"
   },
   {
     "url": "styles.18c935f5f4219e01d654.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4facd0d91a91fbeacdd5128b51db8d49"
+    "revision": "79090e95ea809ea1a1d44881bd21cf0a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
